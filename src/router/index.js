a73ae4016809b3f9045f1e5fe8bb199b78fa454d
Rodrigo Lopez <rodrigoalt15@gmail.com>
2023-05-03 16:52:55 -0500

    feat: added plants component

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../plants/views/HomeView.vue'
-import LoginView from '../auth/components/log-in.component.vue'
-import PlansView from '../auth/components/plans.conponent.vue'
-import RegisterView from '../auth/components/register.component.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../plants/views/HomeView.vue";
+import LoginView from "../auth/components/log-in.component.vue";
+import PlansView from "../auth/components/plans.conponent.vue";
+import RegisterView from "../auth/components/register.component.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/login',
+      path: "/login",
       component: LoginView,
     },
     {
-      path: '/plans',
+      path: "/plans",
       component: PlansView,
     },
     {
-      path: '/register',
+      path: "/register",
       component: RegisterView,
-    }
-  ]
-})
+    },
+  ],
+});
 
-export default router
+export default router;
