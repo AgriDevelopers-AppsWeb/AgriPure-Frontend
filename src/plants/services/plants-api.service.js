5a1e2b9c37a8815b4f9ac0f0bdccfeba6ad83ed7
Rodrigo Lopez <64718167+Rodrigoalt15@users.noreply.github.com>
2023-06-27 10:06:10 -0500

    feat: plant services actualization

--- src/plants/services/plants-api.service.js
+++ src/plants/services/plants-api.service.js
@@ -1,13 +1,13 @@
 import httpCommon from "@/core/services/http-common";
 
 export class PlantsApiService {
-    getAll() {
-        return httpCommon.get('/plants');
-    }
-    getById(id) {
-        return httpCommon.get(`/plants/${id}`);
-    }
-    findByName(name) {
-        return httpCommon.get(`/plants?name=${name}`);
-    }
+  getAll() {
+    return httpCommon.get("/plant");
+  }
+  getById(id) {
+    return httpCommon.get(`/plant/${id}`);
+  }
+  findByName(name) {
+    return httpCommon.get(`/plant?name=${name}`);
+  }
 }
